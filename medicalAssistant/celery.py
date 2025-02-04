d5d5848ce38342696083506a8f29e642a6d94cc3
elihat2022 <cacereselihat@gmail.com>
2025-02-04 15:04:55 -0500

    celery

--- medicalAssistant/celery.py
+++ medicalAssistant/celery.py
@@ -5,3 +5,4 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'medicalAssistant.settings')
 app = Celery('medicalAssistant')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
+app.conf.broker_connection_retry_on_startup = True
